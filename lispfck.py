a270a2ef876ed155a197c8a1e7cec8ef4fb55d03
Halê Valente Silva <halevalente@gmail.com>
2017-11-25 17:07:14 -0200

    adds parser

--- lispfck.py
+++ lispfck.py
@@ -18,3 +18,19 @@ tokens = [
     'CLOSE_P'
 ]
 
+atom = lambda x: x
+term = lambda x: (x,)
+comp = lambda x, y: (x,) + y
+pare = lambda x, y: '()'
+expr = lambda x, y, z: y
+
+
+parser = ox.make_parser([
+    ('expr : OPEN_P term CLOSE_P', expr),
+    ('expr : OPEN_P CLOSE_P', pare),
+    ('term : atom term', comp),
+    ('term : atom', term),
+    ('atom : expr', atom),
+    ('atom : TEXT'; atom),
+    ('atom : NUMBER', atom),
+], tokens)
